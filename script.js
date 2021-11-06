febb74b4c19f414347ec505c54774fbfcb49963e
Ivan Geier <vilajr.ivan@gmail.com>
2021-11-06 16:49:39 -0300

    Implementado os operadores soma, subtracao, multiplicacao e divisao

--- script.js
+++ script.js
@@ -1,26 +1,128 @@
 var display = document.getElementById('display');
 var keys = document.getElementsByClassName('number');
+var operators = document.getElementsByClassName('operator');
+const result = document.getElementsByClassName('result');
 const clear = document.getElementsByClassName('clear');
 
-clear[0].addEventListener('click', () =>{
-    display.innerHTML = '0';
-})
+var firstInput = null;
+var secondInput = null;
+var operator = null;
+
+// EVENT LISTENERS
+
+clear[0].addEventListener('click', clearDisplay)
 
 // Inserir os números selecionados no display
 for (let el of keys) {
-    el.addEventListener('click', () =>{
-        // Não deixa inserir números maiores que 9 digitos
-       if (display.innerText.length < 9) {
-
-            // Se display está zerado substitui pelo número pressionado
-            if (display.innerText == '0') {
-                display.innerText = el.innerText;
-            } else {
-                display.innerText += el.innerText
-            }
-        }
+    el.addEventListener('click', () => {
+        insertNumber(el.innerText);
     })
 }
 
+// Operacoes selecionadas
+for (let op of operators) {
+    op.addEventListener('click', () =>{
+        operator = op.innerText;
+        operation();
+    })
+}
 
+//Igual selecionado
+result[0].addEventListener('click', () => {
+    if (operator) {
+        operation();
+    }
+})
 
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function clearOperatorAndInputs() {
+    firstInput = null;
+    secondInput = null;
+    operator = null;
+}
+
+// FUNCTIONALITIES
+
+function insertNumber(number) {
+    // Não deixa inserir números maiores que 9 digitos
+    if (display.innerText.length < 9) {
+
+        // Se display está zerado substitui pelo número pressionado
+        if (display.innerText == '0') {
+            display.innerText = number;
+        } else {
+            display.innerText += number
+        }
+    }
+}
+
+function operation() {
+    switch (operator) {
+        case '+':
+            if (checkInputs()) {  
+                soma();
+            } else {
+                clearDisplay();
+            }
+            break;
+        
+        case '-':
+            if (checkInputs()) {  
+                subtracao();
+            } else {
+                clearDisplay();
+            }
+            break;
+
+        case 'x':
+            if (checkInputs()) {  
+                multiplicacao();
+            } else {
+                clearDisplay();
+            }
+            break;
+            
+        case '/':
+            if (checkInputs()) {  
+                divisao();
+            } else {
+                clearDisplay();
+            }
+            break;
+        
+
+    }
+}
+
+function checkInputs() {
+    if (firstInput != null && secondInput != null) {
+        return true;
+    } else if (firstInput != null) {
+        secondInput = parseFloat(display.innerText);
+        return true;
+    } else {
+        firstInput = parseFloat(display.innerText);
+        return false;
+    }
+}
+
+function soma(){
+   display.innerText = firstInput + secondInput;
+   clearOperatorAndInputs();
+};
+
+function subtracao(){
+    display.innerText = firstInput - secondInput;
+   clearOperatorAndInputs();
+};
+function multiplicacao(){
+    display.innerText = firstInput * secondInput;
+   clearOperatorAndInputs();
+};
+function divisao(){
+    display.innerText = firstInput / secondInput;
+   clearOperatorAndInputs();
+};
